185970ef0c2ba4669b0bb43f2bbe2a677c459d30
vasyaa <vasyaa@gmail.com>
2021-05-07 07:10:44 -0500

    sum(), struct sum

--- cpp-scratch.cpp
+++ cpp-scratch.cpp
@@ -6,31 +6,41 @@ using namespace std;
 
 // print - tempalte
 namespace Print1 {
-    void print() {
-        std::cout << std::endl;
-    }
+void print() { std::cout << std::endl; }
 
-    template <typename T, typename ... Args>
-    void print(const T& t, Args&&... args) {
-        std::cout << t << " ";
-        print(args...);
-    }
+template <typename T, typename... Args>
+void print(const T& t, Args&&... args) {
+    std::cout << t << " ";
+    print(args...);
 }
+}  // namespace Print1
 
 // print - fold function
 namespace Print2 {
 
-template <typename ... Args>
+template <typename... Args>
 void print(Args&&... args) {
     (std::cout << ... << args) << std::endl;
 }
 
-}
+}  // namespace Print2
 
 namespace Sum1 {
 //    template<typename... V>
 //    double sum(V... v);
 
+// template <typename ... Args>
+// double sum(Args... args);
+
+template <typename T>
+double sum(T t) {
+    return t;
+}
+
+template <typename T, typename... Args>
+double sum(T t, Args... args) {
+    return t + sum(args...);
+}
 }
 
 namespace Sum2 {
@@ -38,43 +48,82 @@ namespace Sum2 {
 //    template <typename ...V>
 //    double sum();
 
-
 }
 
 namespace Sum3 {
 //    template <int ...v>
 //    struct sum;
 
+template <int v, int ...Args>
+struct sum {
+    static const int value = v + sum<Args...>::value;
+};
+
+template <int v>
+struct sum<v> {
+    static const int value = v;
+};
+
+
+
 }
 
+namespace List {
 //////////////////////////////////////////
-//template <typename ... Types>
-//struct list;
+// template <typename ... Types>
+// struct list;
+// method size()
 
-template <typename ...Args>
+template <typename... Args>
 struct list;
 
 template <typename T>
 struct list<T> {
     const T value;
-    explicit list(T  v): value(std::move(v)) {}
+    explicit list(T v) : value(std::move(v)) {}
+    constexpr size_t size() { return 1; }
 };
 
-template <typename T, typename ...Args>
+template <typename T, typename... Args>
 struct list<T, Args...> {
     const T value;
     list<Args...> next;
 
-//    explicit list(T v, Args... args):value(std::move(v)), next(args...) {}
+    //    explicit list(T v, Args... args):value(std::move(v)), next(args...) {}
 
     template <typename U>
-    list(U&& v, Args... args):value(std::forward<U>(v)), next(args...){}
+    explicit list(U&& v, Args... args) : value(std::forward<U>(v)), next(args...) {}
+
+    constexpr size_t size() { return 1 + next.size(); }
 };
 
-template <typename ... Args>
+template <typename... Args>
 list<Args...> make_list(Args... args) {
     return list<Args...>(std::forward<Args>(args)...);
 }
+}  // namespace List
+
+namespace Tuple {
+// template <typename ... Types>
+// tuple
+
+template <typename... Types>
+struct tuple;
+
+template <typename T>
+struct tuple<T> {
+    explicit tuple(T& v) : value(v) {}
+    T value;
+};
+
+template <typename T, typename... Types>
+struct tuple<T, Types...> {
+    explicit tuple(const T& v, Types... args) : value(v), next(args...) {}
+    T value;
+    tuple<Types...> next;
+};
+
+}  // namespace Tuple
 
 // in_set
 
@@ -91,47 +140,52 @@ struct is_same {
 template <typename T>
 struct is_same<T, T> {
     const bool value = true;
-//    enum {value2 = true};
+    //    enum {value2 = true};
 };
 
-}
-
+}  // namespace Is_Same
 
 int main() {
-    {
-        auto p = "asdfgh";
-        Print1::print(1, 2, 3, p);
-        Print2::print(1, 2, 3, p);
-    }
 //    {
-//        auto n = Sum1::sum(1, 2, 3, 4.2, 5.7);
-//        cout << n << endl;
+//        auto p = "asdfgh";
+//        Print1::print(1, 2, 3, p);
+//        Print2::print(1, 2, 3, p);
 //    }
-//    {
-//        Sum3::sum<1,2,3,4,5> s;
-//        auto n = s.value;
-//        cout << n << endl;
-//
-//    }
+        {
+            auto n = Sum1::sum(1, 2, 3, 4.2, 5.7);
+            cout << n << endl;
+        }
+        {
+            auto n = Sum3::sum<1, 2, 3, 4, 5>::value;
+            cout << n << endl;
+            static_assert(Sum3::sum<1, 2, 3, 4, 5>::value == 15, "Value should be 15");
+        }
     {
         auto i = 100;
         auto b = "qwerty";
-        list <int, int, int, int, std::string, std::string> l(1,2,3, i, "asdf", b);
+        List::list<int, int, int, int, std::string, std::string> l(1, 2, 3, i, "asdf", b);
         cout << l.value << " ";
         cout << l.next.value << " ";
         cout << l.next.next.value << " ";
         cout << l.next.next.next.value << " ";
         cout << l.next.next.next.next.value << endl;
         cout << l.next.next.next.next.next.value << endl;
-//        cout << l.next.next.next.next.next << endl;
+        cout << "size " << l.size() << endl;
+        //        cout << l.next.next.next.next.next << endl;
 
-        auto l2 = make_list(1, 2, i, b);
+        auto l2 = List::make_list(1, 2, i, b);
         cout << l2.value << " ";
         cout << l2.next.value << " ";
         cout << l2.next.next.value << " ";
         cout << l2.next.next.next.value << " ";
         std::cout << std::endl;
+        cout << "size " << l2.size() << endl;
+    }
 
+    {
+        Tuple::tuple<int, int> t(1, 2);
+
+        std::cout << t.value << std::endl;
     }
 
     std::cout << is_same<int, std::int32_t>::value << endl;
